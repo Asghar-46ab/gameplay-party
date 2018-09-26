3ce01e14488c747f6ad0f1f01b8cc92be986cd33
Asghar Shiri <514073@edu.rocmn.nl>
2018-09-26 21:32:36 +0200

    .

--- config.php
+++ config.php
@@ -1,9 +1,4 @@
 <?php
 $config['base_url']= "/Applications/XAMPP/xamppfiles/htdocs/gameplay-party/";
 
-<<<<<<< HEAD
-=======
-// IK HOOP DART JIJ DIT NIET ZIET ssss
-
->>>>>>> 208fdb5fa1b7be7aedceffce970fe08d820acf1a
 ?>
