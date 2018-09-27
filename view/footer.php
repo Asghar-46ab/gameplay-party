20f5a7474bb9996f4e54880a0ea9b369e2bc9d9f
Asghar Shiri <514073@edu.rocmn.nl>
2018-09-27 19:53:42 +0200

    footer header

---
+++ view/footer.php
@@ -0,0 +1,10 @@
+
+</div><!--eind van div row-->
+
+    </div><!--eind van div content-->
+
+<!--footer code-->
+
+</body>
+
+</html>
---
+++ view/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- bootstrap css li  -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+        <!--  Font Awesome  -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+        <!-- algemeen style-->
+    <link rel="stylesheet" href="view/assets/style.css">
+
+    <title>Game Play Party</title>
+
+</head>
+
+<body>
+
+<!--page content-->
+<div class="row">
+
+    <div id="content-page" class="col-12">
