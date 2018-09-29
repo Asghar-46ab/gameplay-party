ff4433324b78d9f7ee48b26234f6508c92c31213
Asghar Shiri <514073@edu.rocmn.nl>
2018-09-29 13:23:25 +0200

    updatte

--- view/partials/header.php
+++ view/partials/header.php
@@ -4,7 +4,7 @@
 
 
         <!-- algemeen style-->
-        <link rel="stylesheet" type="text/css" href= 'view/assets/css/style.css'>
+        <link rel="stylesheet" type ="text/css" href='./assets/css/mainStyle.css'>
 
         <!-- Required meta tags -->
     <meta charset="UTF-8">
