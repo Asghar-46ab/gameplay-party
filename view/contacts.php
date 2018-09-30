e8f1eb7c294db272c6769baf51d82333f8407243
Akram <512135@edu.rocmn.nl>
2018-09-30 23:36:10 +0200

    contact pagina

--- view/contacts.php
+++ view/contacts.php
@@ -1,6 +1,46 @@
-
 <?php
 require 'partials/header.php';
-// echo $table;
+?>
+<!-- // echo $table; -->
+<div class="container">
+    <div class="row">
+        <div class="mb-5 mt-5 col">
+            <h1 style="text-align:center">Contact</h1><br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin viverra, risus at rutrum ultrices, erat lorem iaculis ipsum, in maximus ante leo vitae ligula. Nulla vitae dui ut velit convallis tempus nec ac turpis. Sed quam leo, posuere ac elementum eget, ornare vitae leo. Maecenas mattis augue quis mi tempus, eget aliquet sem eleifend. Suspendisse nulla felis, laoreet eget nulla eget, fermentum posuere elit. Cras metus odio, semper at sapien ut, volutpat rhoncus dui. Nullam eget ipsum a justo accumsan lobortis vel eget arcu. Nullam quis fringilla augue.</p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="mb-5 mt-4 col">
+            <p>
+                <h2><strong>Contactgegevens</strong></h2>
+                Tel: (656)-976-4980<br>
+                Email: jack.jones@multiversum.com
+            </p>
+        </div>
+        <div class="mb-5 mt-4 col">
+            <p>
+                <h2><strong>Adresgegevens</strong></h2>
+                Game<br>
+                1861 jan pieterszoon coenstraat<br>
+                69217 maasdriel<br>
+                zeeland
+            </p>
+        </div>
+    </div>
+</div>
+<!-- <hr class="mb-5 mt-5">
+<div class="container-fluid">
+    <div class="embed-responsive embed-responsive-21by9">
+        <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.377988051701!2d5.095329079931542!3d52.09105207578099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c66f65451cd7b3%3A0xa52fd8e5ccf60705!2sJan+Pieterszoon+Coenstraat%2C+Utrecht!5e0!3m2!1snl!2snl!4v1527578964225" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div> -->
+<hr class="mb-5 mt-5">
+
+
+
+
+  </div>
+  <?php
 require 'partials/footer.php';
 ?>
