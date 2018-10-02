2fdb89b0f29892040b9ca5e402e643eb9e496cce
Akram <512135@edu.rocmn.nl>
2018-10-02 17:50:55 +0200

    .
    .

--- view/partials/header.php
+++ view/partials/header.php
@@ -30,7 +30,7 @@
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
 <a class="navbar-brand" href="http://localhost/gameplay-party/ContactsController/Home"><img src="../images/path1803.png" height="55px" alt="X"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
