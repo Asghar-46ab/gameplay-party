836b48febeac90eabc90f7be0a6b1211414df03c
Frank-4a5a <449758@edu.rocmn.nl>
2018-10-04 02:36:10 +0200

    router frank
    misschien kunnen we wat gebruiken van de oude router

---
+++ model/Router_frank.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *
+ */
+class Router
+{
+  function __construct()
+  {
+    //haalt url op
+    $uri = $_SERVER['REQUEST_URI'];
+    //haal url uit elkaar op '/'
+    $exploded = explode('/',$uri);
+    //filter alle lege strings uit
+    $filtered = array_filter($exploded);
+
+    $this->determineDestination($filtered);
+  }
+
+  public function determineDestination($filtered)
+  {
+    var_dump($filtered[2] );
+    //$filtered[1] = gameplay-party = root project
+    //$filtered[2] = gekozen pagina = php pagina in view map
+    //$filtered[3] = classname      = classname en filename
+    //$filtered[4] = method         = method in een class
+    //$filtered[5] = parameters     = parameters voor de method
+    // if ($filtered[1]=='gameplay-party') {
+    //   include($filename);
+    // }
+    $page = $filtered[2];
+
+    $this->sendToDestination($page);
+  }
+
+  public function sendToDestination($page,$classname='',$method='',$params='')
+  {
+    $filename = 'view/' . $page . '.php';
+    include($filename);
+
+    $controller = new $classname($method,$params);
+
+    //$class =  APP_DIR . 'controller/' . $classname . '.php';
+    //require_once($class);
+    //Create object and call method
+    //$obj = new $classname;
+    //die(call_user_func_array(array($obj, $method), $params));
+  }
+
+  public function __destruct()
+  {
+
+  }
+}
