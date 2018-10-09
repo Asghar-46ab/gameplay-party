04001d15ec9867695935c735989674ea9fe28f0b
Akram <512135@edu.rocmn.nl>
2018-10-09 19:57:03 +0200

    .
    .

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-	require_once"config.php";
-	require_once"model/router.php";
+	require_once "config.php";
+	require_once "model/Router.php";
 	define ('APP_DIR', $config['base_url']);
 	$router = new Router();
 	
