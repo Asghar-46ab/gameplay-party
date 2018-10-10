ef51a1e3d7eaf559de062aafc6056595345980ae
Akram <512135@edu.rocmn.nl>
2018-10-10 11:25:50 +0200

    contact
    form

--- view/pages/contact.php
+++ view/pages/contact.php
@@ -1,22 +1,31 @@
 <?php include 'view/partials/header.php';?>
 <div class="row">
-  <div id="sliderHome" class="col-12">
-    <!-- <img class="mySlides" src="../images/sliderImage1.jpg">
-    <img class="mySlides" src="../images/sliderImage2.jpg">
-    <img class="mySlides" src="../images/sliderImage3.jpg"> -->
-
-    <div class="col-12 sliderButtonDiv">
-      <!-- <span class="sliderButton" onclick="changeSlide(1)"></span>
-      <span class="sliderButton" onclick="changeSlide(2)"></span>
-      <span class="sliderButton" onclick="changeSlide(3)"></span> -->
-    </div>
-  </div>
-</div>
-<div class="row">
   <div class="col-3"></div>
   <div id="informatieHome" class="col-6">
-    <h3 class="col-12">Home</h3>
+    <h3 class="col-12">Contact Pagina</h3>
     <span class="col-12">Informatie over het bedrijf, Wat wij doen en waar wij voor staan.</span>
+  </div>
+  <div class="col-3"></div>
+</div>
+
+
+  <div class="col-3"></div>
+  <div id="informatieHome" class="col-6">
+    <h3 class="col-12">Contact Form</h3>
+    <span class="col-12"><div class="container">
+  <form action="/action_page.php">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+    
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
+</div></span>
   </div>
   <div class="col-3"></div>
 </div>
