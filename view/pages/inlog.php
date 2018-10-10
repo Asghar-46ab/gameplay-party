dfb2b49844189f9da726d4e31a4908f84c94eb61
Akram <512135@edu.rocmn.nl>
2018-10-10 12:47:23 +0200

    .

--- view/pages/inlog.php
+++ view/pages/inlog.php
@@ -2,7 +2,7 @@
 <div class="row">
   <div class="col-3"></div>
   <div id="informatieHome" class="col-6">
-    <h3 class="col-12">Contact Pagina</h3>
+    <h3 class="col-12">Inlog Pagina</h3>
     <span class="col-12">Informatie over het bedrijf, Wat wij doen en waar wij voor staan.</span>
   </div>
   <div class="col-3"></div>
@@ -35,5 +35,5 @@
   </div>
   <div class="col-3"></div>
 </div>
-<span id="contactPageCheck"></span>
+<span id="inlogPageCheck"></span>
 <?php include 'view/partials/footer.php';?>
